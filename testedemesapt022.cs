41c36320137bd06c872e9d57fb22f4d47e9f2ba1
Nicoli <nicolisilva@bne.com.br>
2026-06-09 15:34:50 -0300

    Add files via upload

---
+++ testedemesapt022.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+
+class Tabela2
+{
+    static void Main()
+    {
+        double valorPresente = 3800.00;
+        double taxaJuros = 1.25 / 100;
+
+        int mes = 1;
+
+        double rendimento;
+        double rendimentoLiquido;
+        double rendaAcumulada = valorPresente;
+
+        Console.WriteLine("MES\tRENDIMENTO\tLUCRO\t\tACUMULADO");
+
+        while (mes <= 6)
+        {
+            rendimento = valorPresente * Math.Pow(1 + taxaJuros, mes);
+
+            rendimentoLiquido = rendimento - valorPresente;
+
+            rendaAcumulada = rendimento;
+
+            Console.WriteLine(
+                mes + "\tR$ " +
+                rendimento.ToString("F2") +
+                "\tR$ " +
+                rendimentoLiquido.ToString("F2") +
+                "\tR$ " +
+                rendaAcumulada.ToString("F2")
+            );
+
+            mes++;
+        }
+    }
+}
