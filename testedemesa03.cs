97c5000e777a18ed00e3df17f7efb3296009d1b5
Nicoli <nicolisilva@bne.com.br>
2026-06-09 15:33:41 -0300

    Add files via upload

---
+++ testedemesa03.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Tabela2
+{
+    static void Main()
+    {
+        int a = 7;
+        int b = a - 6; // b = 1
+
+        int[] v = new int[8];
+
+        while (b < a)
+        {
+            v[b] = b + a;
+            b = b + 2;
+        }
+
+        Console.WriteLine("qual o vetor:");
+
+        for (int i = 0; i < v.Length; i++)
+        {
+            Console.WriteLine($"v[{i}] = {v[i]}");
+        }
+
+        Console.ReadKey();
+    }
+} 
