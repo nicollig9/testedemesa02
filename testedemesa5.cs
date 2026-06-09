23b931ca2de9f8b0c4f369824401c43ea5debaa4
Nicoli <nicolisilva@bne.com.br>
2026-06-09 15:36:03 -0300

    Add files via upload

---
+++ testedemesa5.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Tabela5
+{
+    static void Main()
+    {
+        double valorFuturo = 7390.61;
+        double taxaJuros = 1.25 / 100;
+        int periodoAnos = 2;
+
+        double valorPresente;
+
+        valorPresente = valorFuturo / Math.Pow(1 + taxaJuros, periodoAnos);
+
+        Console.WriteLine("Valor Futuro: R$ " + valorFuturo);
+        Console.WriteLine("Taxa de Juros: " + (taxaJuros * 100) + "%");
+        Console.WriteLine("Periodo: " + periodoAnos + " anos");
+        Console.WriteLine("Valor Presente: R$ " + valorPresente.ToString("F2"));
+    }
+}
