6ed271e88d8db24d4f3d709d2b85dc578a4061eb
Nicoli <nicolisilva@bne.com.br>
2026-06-09 15:34:03 -0300

    Add files via upload

---
+++ testedemesapt02.cs
@@ -0,0 +1,22 @@
+﻿
+
+using System;
+
+class Tabela1
+{
+    static void Main()
+    {
+        double valorPresente = 1000.00;
+        double taxaJuros = 5.30 / 100;
+        int periodoMeses = 6;
+
+        double valorFuturo;
+
+        valorFuturo = valorPresente * Math.Pow(1 + taxaJuros, periodoMeses);
+
+        Console.WriteLine("Valor Presente: R$ " + valorPresente);
+        Console.WriteLine("Taxa de Juros: " + (taxaJuros * 100) + "%");
+        Console.WriteLine("Período: " + periodoMeses + " meses");
+        Console.WriteLine("Valor Futuro: R$ " + valorFuturo.ToString("F2"));
+    }
+}
